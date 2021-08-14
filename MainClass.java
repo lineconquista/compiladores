0648bab9ff791fab8f67b983dbd12ea5a874eade
Natalia Vares <nataliavares@hotmail.com>
2021-08-14 01:36:45 -0300

    numeros negativos

--- MainClass.java
+++ MainClass.java
@@ -2,25 +2,8 @@ import java.util.Scanner;
 public class MainClass{ 
 	public static void main (String args[]){ 
 	Scanner _key = new Scanner(System.in);
+String a;
 boolean x;
-int i;
-x =true;
-System.out.println("\n teste 1 \n");
-while (x==true) {
-x =false;}
-
-System.out.println("\n teste 2 \n");
-i =0;
-for(i = 0;i<10;i = i + 1) {
-System.out.println(i);}
-
-i =0;
-System.out.println("\n teste 3 \n");
-while (i+1<10) {
-System.out.println(i);i =i + 1;}
-
-System.out.println("\n teste 4 \n");
-do {
-System.out.println(i);i =i - 1;} while (i>0);
-
+if (1==1) {System.out.println("Hello World2");}else {System.out.println("1");}
+if (1<1) {System.out.println("1");}else {System.out.println("Hello World2");}
 	}}
